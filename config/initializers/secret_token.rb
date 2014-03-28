d734baa7e62a80c3dc043ba1602f947421384522
Jason Lewis <jason@decomplecting.org>
2014-03-28 15:59:39 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4PostgresDemo::Application.config.secret_key_base = 'fb9d80df4ca8716a808b2fea24edd2d360a719a97a1fbf04643a458420f599d8c37f5aa73106cc5654c516aab0518bc08e4ee705e49754c4b0b599bc0902bfaf'
